3fa192848b0eede85ff2722bf6ea833beae6be48
HoolAgain <evallis1867@gmail.com>
2024-09-24 09:14:39 -0400

    Test Commit for Project Folder

---
+++ CVGS-PROG3050/CVGS-PROG3050/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CVGS_PROG3050.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
