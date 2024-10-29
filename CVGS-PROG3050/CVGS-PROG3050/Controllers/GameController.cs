542780d565719c071478833fdff687851773f378
Trusun3528 <115508747+Trusun3528@users.noreply.github.com>
2024-10-29 17:04:50 -0400

    Worked on the Display all games
    1. added game view model
    2. added games controller
    3. set up the games page and got it to work

---
+++ CVGS-PROG3050/CVGS-PROG3050/Controllers/GameController.cs
@@ -0,0 +1,19 @@
+﻿using CVGS_PROG3050.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CVGS_PROG3050.Controllers
+{
+    public class GameController : Controller
+    {
+
+        private readonly VaporDbContext _db;
+        public GameController(VaporDbContext context) {
+            _db = context;
+        }
+        public IActionResult AllGamesView()
+        {
+            var games = _db.Games.ToList();
+            return View(games);
+        }
+    }
+}
---
+++ CVGS-PROG3050/CVGS-PROG3050/Models/GameViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CVGS_PROG3050.Models
+{
+    public class GameViewModel
+    {
+        public int GameId { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+
+    }
+}
