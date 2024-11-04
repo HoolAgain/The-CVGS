70536bd082f8c18c1b01511086d4fc7a272fe9cd
Trusun3528 <115508747+Trusun3528@users.noreply.github.com>
2024-11-04 14:12:11 -0500

    Made the events view no longer error out when loading page

---
+++ CVGS-PROG3050/CVGS-PROG3050/Controllers/EventController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CVGS_PROG3050.Controllers
+{
+    public class EventController : Controller
+    {
+        public IActionResult EventView()
+        {
+            return View();
+        }
+    }
+}
